589a715d6f3012de3e74e6cdb4d28a04bd83d7a2
Nailed34 <ilianik34@mail.ru>
2026-06-10 00:59:51 +0300

    fix: удаление файлов мока из сборки

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { rmSync } from 'node:fs'
+import { resolve } from 'node:path'
+
+// Удаляет файл msw после сборки
+const removeMockServiceWorker = () => ({
+  name: 'remove-mock-service-worker',
+  closeBundle() {
+    const workerPath = resolve(__dirname, 'dist', 'mockServiceWorker.js')
+    try {
+      rmSync(workerPath, { force: true })
+      console.log('\n[remove-mock-service-worker] mockServiceWorker.js успешно удален из dist')
+    } catch (e) {
+      console.error('\n[remove-mock-service-worker] Ошибка удаления:', e)
+    }
+  },
+})
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => ({
+  plugins: [
+    react(),
+    command === 'build' && removeMockServiceWorker(), 
+  ],
   server: {
     proxy: {
       "/api": {
@@ -13,4 +32,13 @@ export default defineConfig({
       },
     },
   },
-});
+  // исключение мок данных из сборки
+  build: {
+    rollupOptions: {
+      external: [
+        'src/api/**/mock/**/*',
+        'src/mock.ts'
+      ]
+    }
+  }
+}))
