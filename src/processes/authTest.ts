2165213f89d075d9a62e24bef4be832c519ae05c
VINKOS <46785412+VINKOS7@users.noreply.github.com>
2026-09-16 20:15:35 +0700

    fix: revert some code from authTest.ts

--- src/processes/authTest.ts
+++ src/processes/authTest.ts
@@ -17,18 +17,4 @@ export const authTest = () => beforeEach(() => {
             win.localStorage.setItem(`refresh_token`, "admin-refresh-token");
             win.localStorage.setItem(`access_token`, "admin-access-token");
         })
-
-        cy.visit(RoutesPath.CreateApplication)
-
-        cy.get(`[ux-test-id="${getUXTID("add-application")}"]`).click()
-
-        cy.get(`[ux-test-id="${getUXTID("add-application-preform")}"]`).should('exist')
-
-        cy.get(`[ux-test-id="${getUXTID("application-description")}"]`).should('exist')
-
-        cy.get(`[ux-test-id="${getUXTID("back")}"]`).click()
-
-        cy.get(`[ux-test-id="${getUXTID("add-application")}"]`).click()
-
-        cy.get(`[ux-test-id="${getUXTID("go-write-application-btn")}"]`).click()
 })
